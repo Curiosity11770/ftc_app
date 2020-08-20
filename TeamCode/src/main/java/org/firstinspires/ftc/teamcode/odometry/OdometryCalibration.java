b8bc592042ee3e23de80e7f178a5f1ad0ac80cb3
Katie SB <katescoles-bran23@marlborough.org>
2020-08-20 14:41:51 -0700

    Added reverse to left back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/OdometryCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/OdometryCalibration.java
@@ -173,7 +173,7 @@ public class OdometryCalibration extends LinearOpMode {
         left_front.setDirection(DcMotorSimple.Direction.REVERSE);
         right_front.setDirection(DcMotorSimple.Direction.REVERSE);
         right_back.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        left_back.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addData("Status", "Hardware Map Init Complete");
         telemetry.update();
