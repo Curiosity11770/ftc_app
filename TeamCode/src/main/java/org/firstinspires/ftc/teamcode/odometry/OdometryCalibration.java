4e4a2142d7ec9ca92075ba5ea6129d4143a923ee
Katie SB <katescoles-bran23@marlborough.org>
2020-08-25 10:40:49 -0700

    changed counts per inch to big number

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/OdometryCalibration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/OdometryCalibration.java
@@ -38,7 +38,7 @@ public class OdometryCalibration extends LinearOpMode {
     final double PIVOT_SPEED = 0.5;
 
     //The amount of encoder ticks for each inch the robot moves. THIS WILL CHANGE FOR EACH ROBOT AND NEEDS TO BE UPDATED HERE
-    final double COUNTS_PER_INCH = 400;
+    final double COUNTS_PER_INCH = 1304.45859873;
 
     ElapsedTime timer = new ElapsedTime();
 
